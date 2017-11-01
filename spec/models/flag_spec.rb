3e32e56fd3d6eb1df0eceaf0bc783b97a681d983
Ramon Marques <ramonpm@gmail.com>
2017-10-31 22:08:33 -0200

    adding some model specs

--- spec/models/flag_spec.rb
+++ spec/models/flag_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Flag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:reporter) }
+  it { should belong_to(:infected) }
 end
